55a2726c632c2ecbcc0dc1f47c490747b8d48110
陳欣昱 <yuki280905@gmail.com>
2023-06-07 19:42:30 +0800

    add

---
+++ Web411004215/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web411004215.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Music()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Music(string name, string number)
+        {
+
+            return View();
+        }
+
+    }
+}
